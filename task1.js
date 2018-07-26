696ef156ba3d3163fba4ad1e2a2ad1ed34010c4e
Ivan <nekofurs@gmail.com>
2018-07-27 00:07:30 +0700

    Delete task1.js

--- task1.js
+++
@@ -1,5 +0,0 @@
-function convert (value, fromBase, toBase) {
-	return parseInt(value, fromBase).toString(toBase) ;
-}
-
-process.stdout.write(convert(process.argv[2], process.argv[3], process.argv[4]));
